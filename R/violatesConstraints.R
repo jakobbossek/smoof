59e69851a52eab081f42284143d9cfff921583a1
Jakob Bossek <info@jakobbossek.de>
2014-08-07 15:46:53 +0200

    add violatesConstraints helper

---
+++ R/violatesConstraints.R
@@ -0,0 +1,15 @@
+#' Checks whether constraints are violated.
+#'
+#' @template arg_otf_function
+#' @param values [\code{numeric}]
+#'   List of values.
+#' @return [\code{logical(1)}]
+#FIXME: untested and thus not exported
+#FIXME: if the function expects not only numeric values, we need to pass a _list_ of values
+#       and not a (numeric) vector.
+violatesConstraints = function(fn, values) {
+	assertClass(fn, "otf_function")
+	assertNumber(values, any.missing = FALSE)
+	constraint.fn = attr(fn, "constraint.fn")
+	!all(constraint.fn(values))
+}
