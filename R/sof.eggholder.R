038dfab3a989dd0dfb8e284d94894fbfcd91dfd2
Jakob Bossek <info@jakobbossek.de>
2015-02-19 10:44:51 +0100

    add eggholder function

---
+++ R/sof.eggholder.R
@@ -0,0 +1,20 @@
+#' Drop-Wave function
+#'
+#' @export
+makeEggholderFunction = function() {
+    makeSingleObjectiveFunction(
+        name = "Eggholder function",
+        fn = function(x) {
+            -(x[2] + 47) * sin(sqrt(abs(x[2] + x[1] / 2 + 47))) - x[1] * sin(sqrt(abs(x[1] - (x[2] + 47))))
+        },
+        par.set = makeNumericParamSet(
+            len = 2L,
+            id = "x",
+            lower = c(-512, -512),
+            upper = c(512, 512),
+            vector = FALSE
+        ),
+        global.opt.params = list("x1" = 512, "x2" = 404.2319),
+        global.opt.value = -959.6407
+    )
+}
