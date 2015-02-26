357238fb673846da8033e978377b56788a271582
Jakob Bossek <info@jakobbossek.de>
2015-02-26 15:02:46 +0100

    minor changes to helper

--- helper.R
+++ helper.R
@@ -10,13 +10,12 @@ fn1 = makeHimmelblauFunction()
 fn2 = makeSphereFunction(dimensions = 2L)
 fn3 = makeSchwefelFunction(dimensions = 2L)
 
-
-pdf("smoof_funs_example.pdf", width = 20, height = 6, title = "Examplary functions from our test set")
+png("smoof_funs_example.png", width = 960, height = 300, title = "Examplary functions from our test set")
 op = par()
 par(mfrow = c(1, 3))
-plot3D(fn1, length.out = 200L, contour = TRUE, image = FALSE, facets = TRUE)
-plot3D(fn2, length.out = 200L, contour = TRUE, image = FALSE, facets = TRUE)
-plot3D(fn3, length.out = 200L, contour = TRUE, image = FALSE, facets = TRUE)
+plot3D(fn1, length.out = 50L, contour = TRUE, image = FALSE, facets = TRUE)
+plot3D(fn2, length.out = 50L, contour = TRUE, image = FALSE, facets = TRUE)
+plot3D(fn3, length.out = 50L, contour = TRUE, image = FALSE, facets = TRUE)
 par(op)
 dev.off()
 
