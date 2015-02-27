570246f85c0bbca4ab133a17a6c1483b6db4e722
Jakob Bossek <info@jakobbossek.de>
2015-02-27 15:03:53 +0100

    add zdt2, zdt3 and zdt4

---
+++ R/mof.zdt3.R
@@ -0,0 +1,50 @@
+#' ZDT3 function generator.
+#'
+#' Builds and returns the two-objective ZDT3 test problem. For \eqn{m} objective it
+#' is defined as follows
+#' \deqn{f(\mathbf{x}) = \left(f_1(\mathbf{x}_1), f_2(\mathbf{x})\right)}
+#' with
+#' \deqn{f_1(\mathbf{x}_1) = \mathbf{x}_1, f_2(\mathbf{x}) = g(\mathbf{x}) h(f_1(\mathbf{x}_1), g(\mathbf{x}))}
+#' where
+#' \deqn{g(\mathbf{x}) = 1 + \frac{9}{m - 1} \sum_{i = 2}^m \mathbf{x}_i, h(f_1, g) = 1 - \sqrt{\frac{f_1(\mathbf{x})}{g(\mathbf{x})}} - \left(\frac{f_1(\mathbf{x})}{g(\mathbf{x})}\right)\sin(10\pi f_1(\mathbf{x}))}
+#' and \eqn{\mathbf{x}_i \in [0,1], i = 1, \ldots, m}.
+#' This function has some discontinuities in the Pareto-optimal front introduced
+#' by the sine term in the \eqn{h} function (see above). The front consists of
+#' multiple convex parts.
+#'
+#' @references E. Zitzler, K. Deb, and L. Thiele. Comparison of Multiobjective
+#' Evolutionary Algorithms: Empirical Results. Evolutionary Computation, 8(2):173-195, 2000
+#'
+#' @param dimensions [\code{integer(1)}]\cr
+#'   Number of decision variables.
+#' @return [\code{smoof_multi_objective_function}]
+#' @export
+makeZDT3Function = function(dimensions) {
+    assertNumber(dimensions, lower = 2L, na.ok = FALSE)
+    force(dimensions)
+
+    # define the two-objective ZDT1 function
+    fn = function(x) {
+        stopifnot(length(x) == dimensions)
+        n = length(x)
+        f1 = x[1]
+        g = 1 + 9 * sum(x[2:n]) / (n - 1)
+        h = 1 - sqrt(f1 / g) - (f1 / g) * sin(10 * pi * f1)
+        f2 = g * h
+        return(c(f1, f2))
+    }
+
+    makeMultiObjectiveFunction(
+        name = "ZDT3 function",
+        description = "Zitzler et al. function 3",
+        fn = fn,
+        par.set =  makeNumericParamSet(
+            len = dimensions,
+            id = "x",
+            lower = rep(0, dimensions),
+            upper = rep(1, dimensions),
+            vector = FALSE
+        ),
+        n.objectives = 2L
+    )
+}
---
+++ R/mof.zdt4.R
@@ -0,0 +1,49 @@
+#' ZDT4 function generator.
+#'
+#' Builds and returns the two-objective ZDT4 test problem. For \eqn{m} objective it
+#' is defined as follows
+#' \deqn{f(\mathbf{x}) = \left(f_1(\mathbf{x}_1), f_2(\mathbf{x})\right)}
+#' with
+#' \deqn{f_1(\mathbf{x}_1) = \mathbf{x}_1, f_2(\mathbf{x}) = g(\mathbf{x}) h(f_1(\mathbf{x}_1), g(\mathbf{x}))}
+#' where
+#' \deqn{g(\mathbf{x}) = 1 + 10 (m - 1) + \sum_{i = 2}^{m} (\mathbf{x}_i^2 - 10\cos(4\pi\mathbf{x}_i)), h(f_1, g) = 1 - \sqrt{\frac{f_1(\mathbf{x})}{g(\mathbf{x})}}}
+#' and \eqn{\mathbf{x}_i \in [0,1], i = 1, \ldots, m}.
+#' This function has many Pareto-optimal fronts and is thus suited to test the
+#' algorithms ability to tackle multimodal problems.
+#'
+#' @references E. Zitzler, K. Deb, and L. Thiele. Comparison of Multiobjective
+#' Evolutionary Algorithms: Empirical Results. Evolutionary Computation, 8(2):173-195, 2000
+#'
+#' @param dimensions [\code{integer(1)}]\cr
+#'   Number of decision variables.
+#' @return [\code{smoof_multi_objective_function}]
+#' @export
+makeZDT4Function = function(dimensions) {
+    assertNumber(dimensions, lower = 2L, na.ok = FALSE)
+    force(dimensions)
+
+    # define the two-objective ZDT1 function
+    fn = function(x) {
+        stopifnot(length(x) == dimensions)
+        n = length(x)
+        f1 = x[1]
+        g = 1 + 10 * (n - 1) + sum(x[2:n]^2 - 10 * cos(4 * pi * x[2:n]))
+        h = 1 - sqrt(f1 / g)
+        f2 = g * h
+        return(c(f1, f2))
+    }
+
+    makeMultiObjectiveFunction(
+        name = "ZDT4 function",
+        description = "Zitzler et al. function 4",
+        fn = fn,
+        par.set =  makeNumericParamSet(
+            len = dimensions,
+            id = "x",
+            lower = rep(0, dimensions),
+            upper = rep(1, dimensions),
+            vector = FALSE
+        ),
+        n.objectives = 2L
+    )
+}
