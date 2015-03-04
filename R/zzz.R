1da2cde7ceab6e6a48f3a0a27676ee15e0169089
Jakob Bossek <info@jakobbossek.de>
2015-03-04 19:55:13 +0100

    no more dependency on soobench

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,5 @@
 #' @import ggplot2
 #' @import RColorBrewer
 #' @import plot3D
-#' @import soobench
 #' @importFrom emoa nondominated_points
 NULL
--- tests/testthat/test_makeSingleObjectiveFunctionFromSOOFunction.R
+++
@@ -1,8 +0,0 @@
-context("import from soobench")
-
-test_that("makeSingleObjectiveFunctionFromSOOFunction works as expected", {
-    fn = makeSingleObjectiveFunctionFromSOOFunction("ackley", dimensions = 2L)
-    expect_true(grepl("Ackley", getName(fn)))
-    expect_equal(getNumberOfParameters(fn), 2L)
-    expect_true(is.function(fn$fn))
-})
