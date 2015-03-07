a03a5abeebd75f8ea41e06fde00af96616b386db
Jakob Bossek <info@jakobbossek.de>
2015-03-07 12:56:16 +0100

    add Bartels Conn, Chung Reynolds, Periodic and Powell-Sum functions (as well as Pen Holder function to todo-files)

---
+++ R/sof.chung.reynolds.R
@@ -0,0 +1,37 @@
+#' Chung Reynolds Function
+#'
+#' The defintion is given by
+#' \deqn{f(\mathbf{x}) = \left(\sum_{i=1}^{n} \mathbf{x}_i^2\right)^2}
+#' with box-constraings \eqn{\mathbf{x}_i \in [-100, 100], i = 1, \ldots, n}.
+#'
+#' @references C. J. Chung, R. G. Reynolds, CAEP: An Evolution-Based Tool for
+#' Real-Valued Func- tion Optimization Using Cultural Algorithms, International
+#' Journal on Artificial In- telligence Tool, vol. 7, no. 3, pp. 239-291, 1998.
+#'
+#' @template arg_dimensions
+#' @template ret_smoof_single
+#' @export
+makeChungReynoldsFunction = function(dimensions) {
+  assertCount(dimensions)
+  makeSingleObjectiveFunction(
+    name = paste(dimensions, "-d Chung Reynolds Function", sep = ""),
+    fn = function(x) {
+      sum(x^2)^2
+    },
+    par.set = makeNumericParamSet(
+      len = dimensions,
+      id = "x",
+      lower = rep(-100, dimensions),
+      upper = rep(100, dimensions),
+      vector = FALSE
+    ),
+    tags = attr(makeChungReynoldsFunction, "tags"),
+    global.opt.params = rep(0, dimensions),
+    global.opt.value = 0
+  )
+}
+
+class(makeChungReynoldsFunction) = c("function", "smoof_generator")
+attr(makeChungReynoldsFunction, "name") = c("Chung Reynolds Function")
+attr(makeChungReynoldsFunction, "type") = c("single-objective")
+attr(makeChungReynoldsFunction, "tags") = c("unimodal", "continuous", "differentiable", "scalable")
---
+++ todo-files/sof.pen.holder.R
@@ -0,0 +1,43 @@
+#' Pen Holder Function
+#'
+#' Single-objective two-dimensional test function. The formula is given as
+#' \deqn{f(\mathbf{x}) = -\exp(|\cos(\mathbf{x}_1)\cos(mathbf{x}_2)\exp(1 - \sqrt{\mathbf{x}_1^2 + \mathbf{x}_2^2}/\pi)|^{-1})}
+#' subject to the constraints \eqn{\mathbf{x}_i \in [-11, 11], i = 1, 2.}
+#'
+#' @references S. K. Mishra, Global Optimization By Differential Evolution and
+#' Particle Swarm Methods: Evaluation On Some Benchmark Functions, Munich Research
+#' Papers in Economics.
+#'
+#' @template ret_smoof_single
+#' @export
+#FIXME: global optimum not approximated good enough by formula
+makePenHolderFunction = function() {
+  makeSingleObjectiveFunction(
+    name = "Pen Holder Function",
+    fn = function(x) {
+      a = exp(abs(1 - (sqrt(sum(x^2)) / pi)))
+      b = abs(prod(cos(x)) * a)
+      -exp(1 / b)
+    },
+    par.set = makeNumericParamSet(
+      len = 2L,
+      id = "x",
+      lower = c(-11, -11),
+      upper = c(11, 11),
+      vector = FALSE
+    ),
+    tags = attr(makePenHolderFunction, "tags"),
+    global.opt.params = matrix(
+      c(9.646168, 9.646168,
+        -9.646168, 9.646168,
+        9.646168, -9.646168,
+        -9.646168, -9.646168),
+      ncol = 2L, byrow = TRUE),
+    global.opt.value = -0.96354
+  )
+}
+
+class(makePenHolderFunction) = c("function", "smoof_generator")
+attr(makePenHolderFunction, "name") = c("Pen Holder Function")
+attr(makePenHolderFunction, "type") = c("single-objective")
+attr(makePenHolderFunction, "tags") = c("continuous", "differentiable", "non-separable", "non-scalable", "multimodal")
