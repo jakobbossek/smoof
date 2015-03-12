e3f660c094d93900f1eeee9375e2ec68a3b62409
Jakob Bossek <info@jakobbossek.de>
2015-03-12 10:49:13 +0100

    add simple script to generate plots of all 2D BBOB functions

---
+++ bbob_generate_images.R
@@ -0,0 +1,17 @@
+library(devtools)
+library(plot3D)
+
+load_all(".")
+
+fids = 1:24
+iid = 1L
+dimension = 2L
+
+# save the plot of each fn as 'bbob/{fn}.pdf'
+for (fid in fids) {
+  bbob.fn = makeBBOBFunction(dimension = dimension, fid = fid, iid = iid)
+  file.name = paste("bbob/", getName(bbob.fn), "pdf", sep = ".")
+  pdf(file.name)
+  plot3D(bbob.fn, title = getName(bbob.fn))
+  dev.off()
+}
