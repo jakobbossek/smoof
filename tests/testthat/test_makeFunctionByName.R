93142a12249ac2ecaa2839d93bf92ac87f37b955
Jakob Bossek <info@jakobbossek.de>
2015-11-05 14:28:21 +0100

    add makeFunctionByName

---
+++ tests/testthat/test_makeFunctionByName.R
@@ -0,0 +1,26 @@
+context("makeFunctionByName helper")
+
+test_that("makeFunctionByName helper should work as expected", {
+  # basic checks
+  fn = makeFunctionByName("Ackley Function", dimensions = 2L)
+  expect_is(fn, "smoof_function")
+  expect_true(grepl("ackley", getName(fn), ignore.case = TRUE))
+
+  # check if 2D function is created with dimensions = 2 and no dimensions
+  # attribute at all
+  fn = makeFunctionByName("Branin Function", dimensions = 2L)
+  expect_is(fn, "smoof_function")
+  expect_true(grepl("branin", getName(fn), ignore.case = TRUE))
+  fn = makeFunctionByName("Branin Function")
+  expect_is(fn, "smoof_function")
+  expect_true(grepl("branin", getName(fn), ignore.case = TRUE))
+  expect_error(makeFunctionByName("Branin Function", dimensions = 3L))
+
+  # test in combination with filtering
+  funs = lapply(as.list(filterFunctionsByTags(c("multimodal", "scalable"))),
+    function(fun.name) {
+      makeFunctionByName(fun.name, dimensions = 2L)
+    }
+  )
+  expect_list(funs, types = "smoof_function")
+})
