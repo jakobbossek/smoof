939b70e0a96ed08c8c3710f1cd4479027f9a5ca9
Jakob Bossek <info@jakobbossek.de>
2016-06-11 15:57:31 +0200

    add mixed parameter Zhou2011 function

---
+++ R/sof.zhou2011.R
@@ -0,0 +1,56 @@
+#' @title
+#' Zhou2011 function.
+#'
+#' @description Mixed parameter space with one discrete parameter \eqn{x_1 \in \{1, 2, 3, 4, 5\}}
+#' and two numerical parameters \eqn{x_1, x_2 \in [0, 1]}. The function is defined
+#' as follows:
+#' \deqn{
+#' f(\mathbf{x}) = \\
+#' \sin(2\pi x_3 - \pi) + 7 \sin^2(2 \pi x_2 - \pi) \, if \, x_1 = 1 \\
+#' \sin(2\pi x_3 - \pi) + 7 \sin^2(2 \pi x_2 - \pi) + 12 \sin(2 \pi x_3 - \pi) \, if \, x_1 = 2 \\
+#' \sin(2\pi x_3 - \pi) + 7 \sin^2(2 \pi x_2 - \pi) + 0.5 \sin(2 \pi x_3 - \pi) \, if \, x_1 = 3 \\
+#' \sin(2\pi x_3 - \pi) + 7 \sin^2(2 \pi x_2 - \pi) + 8.0 \sin(2 \pi x_3 - \pi) \, if \, x_1 = 4 \\
+#' \sin(2\pi x_3 - \pi) + 7 \sin^2(2 \pi x_2 - \pi) + 3.5 \sin(2 \pi x_3 - \pi) \, if \, x_1 = 5.
+#' }
+#'
+#' @references See \url{https://cfwebprod.sandia.gov/cfdocs/CompResearch/docs/MixedSurrSAND2012-0491.pdf}.
+#'
+#' @template ret_smoof_single
+#' @export
+makeZhou2011Function = function() {
+  makeSingleObjectiveFunction(
+    name = "Zhou2011 Function",
+    id = paste0("Zhou2011_3d"),
+    fn = function(x) {
+      assertList(x)
+      x1 = x$x1
+      x2 = x$x2
+      x3 = x$x3
+      assertChoice(x1, choices = c("1", "2", "3", "4", "5"))
+      assertNumber(x2, lower = 0, upper = 1)
+      assertNumber(x3, lower = 0, upper = 1)
+
+      a = sin(2 * pi * x3 - pi)
+      b = 7 * sin(2 * pi * x2 - pi)^2
+      facs = c(0, 12.0, 0.5, 8.0, 3.5)
+
+      fac = facs[as.integer(x1)]
+      val = a + b + fac * sin(2 * pi * x3 - pi)
+      return(val)
+    },
+    par.set = makeParamSet(
+      makeDiscreteParam("x1", values = c("1", "2", "3", "4", "5")),
+      makeNumericParam("x2", lower = 0, upper = 1),
+      makeNumericParam("x3", lower = 0, upper = 1)
+    ),
+    tags = attr(makeComplexFunction, "tags"),
+    has.simple.signature = FALSE
+    # global.opt.params = c(1, 0),
+    # global.opt.value = 0
+  )
+}
+
+class(makeComplexFunction) = c("function", "smoof_generator")
+attr(makeComplexFunction, "name") = c("Zhou2014")
+attr(makeComplexFunction, "type") = c("single-objective")
+attr(makeComplexFunction, "tags") = c("single-objective", "discontinuous", "multimodal")
